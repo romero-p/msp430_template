da24912c211161f0883d14cd4a67143b18da0d83
Mariana Romero <marianaromero.p@hotmail.com>
2025-07-04 07:46:13 +0200

    Working simple cycle counter (in CCS)

--- src/main.c
+++ src/main.c
@@ -3,17 +3,14 @@
 
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
-    PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
-                                            // to activate previously configured port settings
-    P1DIR |= 0x01;                          // Set P1.0 to output direction
+    volatile unsigned count = 0;
+    volatile int x = 0;
 
-    for(;;) {
-        volatile unsigned int i;            // volatile to prevent optimization
+    TA0CTL |= TASSEL__SMCLK | MC__CONTINOUS | TACLR; //Set up timer A with SMCKL as the clock source, continous mode and reset timer
 
-        P1OUT ^= 0x01;                      // Toggle P1.0 using exclusive-OR
+    unsigned current_ts = TA0R; //Get the current value of the timer
+    x = x + 1; //some instruction
+    count = TA0R - current_ts; //Substract to get the difference in time aka the cycle count of the instruction
 
-        i = 50000;                          // SW Delay
-        do i--;
-        while(i != 0);
-    }
+    while(1); //debugging purposes
 }
